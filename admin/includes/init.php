b52f58a1b96bdc702ac3dfc528d4bfb4c104378d
Harrison Max-Wilson <H.MaxWilson@hotmail.com>
2022-03-15 19:56:35 +0000

    Building Direction Paths - Lecture 107

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,4 +1,8 @@
 <?php 
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+
+defined('SITE_PATH') ? null : define('SITE_PATH' , 'D:' . DS .  'XAMPP' . DS . 'htdocs' . DS . 'gallery');
+defined('INCLUDES_PATH') ? null : define('INCLUDES_PATH' , SITE_PATH . DS .  'admin' . DS . 'includes');
 
 require_once("functions.php");
 require_once("new_config.php");
