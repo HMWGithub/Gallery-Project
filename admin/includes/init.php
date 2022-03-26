67a6e084676a9c1e00fcbc5fd0ded2c4758dabde
Harrison Max-Wilson <H.MaxWilson@hotmail.com>
2022-03-26 17:37:19 +0000

    Online site root path - Lecture 222

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,7 +1,8 @@
 <?php 
   defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-  defined('SITE_PATH') ? null : define('SITE_PATH', 'D:' . DS .  'XAMPP' . DS . 'htdocs' . DS . 'gallery');
+  define('SITE_PATH', $_SERVER['DOCUMENT_ROOT'] . DS . 'gallery'); // Live
   defined('INCLUDES_PATH') ? null : define('INCLUDES_PATH', SITE_PATH . DS . 'admin' . DS . 'includes');
+  defined('IMAGES_PATH') ? null : define('IMAGES_PATH', SITE_PATH . DS . 'admin' . DS . 'images');
 
   require_once(INCLUDES_PATH.DS."functions.php");
   require_once(INCLUDES_PATH.DS."new_config.php");
